4572307b162dacb879ee0d45e82fac744ad80d61
Rzxprtcl <rhamcesjhester.castrodes@gmail.com>
2026-04-05 02:28:45 +0800

    Refactor script.js to use DOMContentLoaded event

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-const FORM_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSfOIAed-_b1WV4yQkaM0_hlSWh9da5BQUKABYPf-ZrqW0EYhw/viewform";
+document.addEventListener("DOMContentLoaded", () => {
 
-const DASHBOARD_LINK = "https://docs.google.com/spreadsheets/d/1VOfLQfEfvKt4qTMJq3HsLVlu2UjlvmQV5F4O8EQNPhw/edit?resourcekey=&gid=1025882678#gid=1025882678";
+    const FORM_LINK = "https://docs.google.com/forms/d/e/1FAIpQLSfOIAed-_b1WV4yQkaM0_hlSWh9da5BQUKABYPf-ZrqW0EYhw/viewform";
 
-document.getElementById("submitBtn").onclick = () => {
-    if (confirm("Proceed to submit LoA?")) {
-        window.open(FORM_LINK, "_blank");
-    }
-};
+    const DASHBOARD_LINK = "https://docs.google.com/spreadsheets/d/1VOfLQfEfvKt4qTMJq3HsLVlu2UjlvmQV5F4O8EQNPhw/edit";
 
-document.getElementById("dashboardBtn").onclick = () => {
-    window.open(DASHBOARD_LINK, "_blank");
-};
+    document.getElementById("submitBtn").onclick = () => {
+        if (confirm("Proceed to submit LoA?")) {
+            window.open(FORM_LINK, "_blank");
+        }
+    };
 
-document.getElementById("notice").onclick = () => {
-    document.getElementById("notice").classList.toggle("expanded");
-};
+    document.getElementById("dashboardBtn").onclick = () => {
+        window.open(DASHBOARD_LINK, "_blank");
+    };
+
+    document.getElementById("notice").onclick = () => {
+        document.getElementById("notice").classList.toggle("expanded");
+    };
+
+});
